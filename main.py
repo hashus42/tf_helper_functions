cec86cf9897ace1cd52591132e39c64056d4b3ef
hashus42 <hsnhsyntrkmn@gmail.com>
2023-02-24 09:17:30 +0300

    tf helper functions
    Helper functions for tensorflow

---
+++ main.py
@@ -0,0 +1,169 @@
+# This will be a helper functions source.
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix
+from mlxtend.plotting import plot_confusion_matrix
+import datetime
+import zipfile
+import os
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+
+def load_and_prep_image(filename, img_shape=224, normalize=True):
+    img = tf.io.read_file(filename)
+
+    img = tf.image.decode_jpeg(img)
+
+    img = tf.image.resize(img, [img_shape, img_shape])
+
+    if normalize:
+        return img / 255.
+    else:
+        return img
+
+
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=18, norm=True, savefig=False):
+    """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+    If classes is passed, confusion matrix will be labelled, if not, integer class values
+    will be used.
+    Args:
+      y_true: Array of truth labels (must be same shape as y_pred).
+      y_pred: Array of predicted labels (must be same shape as y_true).
+      classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+      figsize: Size of output figure (default=(10, 10)).
+      text_size: Size of output figure text (default=15).
+      norm: normalize values or not (default=True).
+      savefig: save confusion matrix to file (default=False).
+
+    Returns:
+      A labelled confusion matrix plot comparing y_true and y_pred.
+    Example usage:
+      make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                            y_pred=y_preds, # predicted labels
+                            classes=class_names, # array of class label names
+                            figsize=(15, 15),
+                            text_size=10)
+    """
+
+    cm = confusion_matrix(y_true, y_pred)
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+
+    fig, ax = plot_confusion_matrix(figsize=figsize,
+                                    conf_mat=cm,
+                                    show_absolute=True,
+                                    show_normed=norm,
+                                    colorbar=True,
+                                    class_names=classes)
+
+    plt.xlabel("Predictions", fontsize=text_size)
+    plt.ylabel("Actuals", fontsize=text_size)
+    plt.title("Confusion Matrix", fontsize=text_size)
+    plt.show()
+
+    if savefig:
+        fig.savefig("confusion_matrix.png")
+
+
+def pred_and_plot(model, filename, class_names):
+    img = load_and_prep_image(filename)
+
+    pred = model.predict(tf.expand_dims(img, axis=0))
+
+    if len(pred[0]) > 1:
+        pred_class = class_names[pred.argmax()]
+    elif len(pred[0][0]) > 1:
+        pred_class = class_names[pred[0].argmax]
+    else:
+        pred_class = class_names[int(tf.round(pred)[0][0])]
+
+    plt.imshow(img)
+    plt.title(f"Prediction: {pred_class}")
+    plt.axis(False)
+
+
+def crate_tensorboard_callback(dir_name, experiment_name):
+    log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+
+    print(f"Saving Tensorboard log files to: {log_dir}")
+    return tensorboard_callback
+
+
+def plot_loss_curves(history):
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
+
+    epochs = range(len(loss))
+
+    plt.plot(epochs, loss, label="training_loss")
+    plt.plot(epochs, val_loss, label="val_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    plt.plot(epochs, accuracy, label="accuracy")
+    plt.plot(epochs, val_accuracy, label="val_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+
+def compare_history(original_history, new_history, initial_epochs=5):
+    acc = original_history.history["accuracy"]
+    loss = original_history.history["loss"]
+
+    val_acc = original_history.history["val_accuracy"]
+    val_loss = original_history.history["val_loss"]
+
+    total_acc = acc + new_history.history["accuracy"]
+    total_loss = loss + new_history.history["loss"]
+
+    total_val_acc = val_acc + new_history.history["val_accuracy"]
+    total_val_loss = val_loss + new_history.history["val_loss"]
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label="Training Accuracy")
+    plt.plot(total_val_acc, label="Validation Accuracy")
+    plt.plot([initial_epochs - 1, initial_epochs - 1],
+             plt.ylim(), label="Start Fine Tuning")
+    plt.legend(loc="lower right")
+    plt.title("Training and Validation Accuracy")
+
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label="Training Loss")
+    plt.plot(total_val_loss, label="Validation Loss")
+    plt.plot([initial_epochs - 1, initial_epochs - 1],
+             plt.ylim(), label="Start Fine Tuning")
+    plt.legend(loc="upper right")
+    plt.title("Training and Validation Loss")
+    plt.xlabel("epoch")
+    plt.show()
+
+
+def unzip_data(filename):
+    zip_ref = zipfile.ZipFile(filename, "r")
+    zip_ref.extractall()
+    zip_ref.close()
+
+
+def walk_through_dir(dir_path):
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+
+def calculate_results(y_true, y_pred):
+    model_accuracy = accuracy_score(y_true, y_pred) * 100
+
+    model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+    model_results = {"accuracy": model_accuracy,
+                     "precision": model_precision,
+                     "recall": model_recall,
+                     "f1": model_f1}
+
+    return model_results
